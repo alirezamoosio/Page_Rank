4f8fdf08b23f3e3e757c3ac265290bc9a7e31a00
amirsaeedr <amirsaeedranjbar@gmail.com>
2018-08-14 17:45:34 +0430

    add refrence calculator

--- src/main/java/ir/nimbo/pagerank/RankCalculator.java
+++ src/main/java/ir/nimbo/pagerank/RankCalculator.java
@@ -11,17 +11,16 @@ import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
 import scala.Tuple2;
 
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class RankCalculator {
     private static final double DAMPING_FACTOR = 0.85;
@@ -31,9 +30,10 @@ public class RankCalculator {
     private static String pageRankName = "pageRank";
     private Configuration hbaseConf;
     private JavaSparkContext sparkContext;
+    private static Logger logger = Logger.getLogger("error");
 
     RankCalculator(String appName, String master) {
-        String[] jars = {"/home/alireza/Page_Rank/target/Page_Rank-1.0-SNAPSHOT-jar-with-dependencies.jar"};
+        String[] jars = {"/home/amirsaeed/Page_Rank/target/Page_Rank-1.0-SNAPSHOT-jar-with-dependencies.jar"};
         SparkConf sparkConf = new SparkConf().setAppName(appName).setMaster(master).setJars(jars);
         sparkContext = new JavaSparkContext(sparkConf);
         hbaseConf = HBaseConfiguration.create();
@@ -43,103 +43,72 @@ public class RankCalculator {
         hbaseConf.set(TableInputFormat.SCAN_COLUMN_FAMILY, "context");
     }
 
+
     public void calculate() {
-        JavaPairRDD<String, Value> input = getFromHBase();
-        JavaPairRDD<String, Value> result = getResult(input);
+        JavaPairRDD<String, HashSet<String>> input = getFromHBase();
+        JavaPairRDD<String, Integer> result = getResult(input);
         writeToHBase(result);
     }
 
-    JavaPairRDD<String, Value> getFromHBase() {
+
+    JavaPairRDD<String, HashSet<String>> getFromHBase() {
         JavaPairRDD<ImmutableBytesWritable, Result> read =
                 sparkContext.newAPIHadoopRDD(hbaseConf, TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
         return read.mapToPair(pair -> {
             String key = Bytes.toString(pair._1.get());
             String serializedMap = Bytes.toString(pair._2.getColumnLatestCell(familyName.getBytes(), outLinksName.getBytes()).getValue());
-            Double pageRank = Bytes.toDouble(pair._2.getColumnLatestCell(familyName.getBytes(), pageRankName.getBytes()).getValue());
             Gson gson = new Gson();
             if (serializedMap.contains("www.livejournal.com"))
                 return null;
             try {
-                Map<String, String> outLinks = gson.fromJson(serializedMap, Map.class);
-                return new Tuple2<>(key, new Value(outLinks.keySet(), pageRank));
+                HashMap<String, String> outLinks = gson.fromJson(serializedMap, HashMap.class);
+                HashSet<String> outLinkSet = new HashSet<>(outLinks.keySet());
+                return new Tuple2<>(key, outLinkSet);
             } catch (JsonSyntaxException e) {
-                e.printStackTrace();
-                System.out.println(serializedMap);
+                e.getMessage();
                 return null;
             }
         });
     }
 
-    public JavaPairRDD<String, String> testRead() {
-        JavaPairRDD<ImmutableBytesWritable, Result> read =
-                sparkContext.newAPIHadoopRDD(hbaseConf, TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
-        return read.mapToPair(pair -> {
-            String key = Bytes.toString(pair._1.get());
-            String value = Bytes.toString(pair._2.getColumnLatestCell("context".getBytes(), "f1".getBytes()).getValue());
-            return new Tuple2<>(key, value);
-        });
-    }
-
-    public void testWrite(JavaPairRDD<String, String> toWrite) {
-
-        JavaPairRDD<ImmutableBytesWritable, Put> toPut = toWrite.mapToPair(input -> {
-            Put put = new Put(input._1.getBytes());
-            put.addColumn("context".getBytes(), "f1".getBytes(), ("new" + input._2).getBytes());
-            return new Tuple2<>(new ImmutableBytesWritable(), put);
-        });
-        try {
-            Job job = Job.getInstance(hbaseConf);
-            job.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "test");
-            job.setOutputFormatClass(TableOutputFormat.class);
-            toPut.saveAsNewAPIHadoopDataset(job.getConfiguration());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private JavaPairRDD<String, Value> getResult(JavaPairRDD<String, Value> input) {
-        JavaPairRDD<String, Value> mapped;
-        for (int i = 0; i < ITERATION_NUMBER; i++) {
-            mapped = input.flatMapToPair(key -> {
-                List<Tuple2<String, Value>> result = new ArrayList<>();
-                for (String url : key._2.outLinks) {
-                    result.add(new Tuple2<>(url, new Value(null, DAMPING_FACTOR * key._2.pageRank / key._2.outLinks.size())));
+    JavaPairRDD<String, Integer> getResult(JavaPairRDD<String, HashSet<String>> input) {
+        JavaPairRDD<String, Integer> mapped;
+        JavaPairRDD<String, Integer> result;
+        mapped = input.flatMapToPair(key -> {
+            List<Tuple2<String, Integer>> resultList = new ArrayList<>();
+//            System.out.println(key._1);
+            try {
+                if (key._2 != null) {
+                    if (!key._2.isEmpty())
+                        for (String url : key._2) {
+                            resultList.add(new Tuple2<>(url, 1));
+                        }
                 }
-                result.add(new Tuple2<>(key._1, new Value(key._2.outLinks, 1 - DAMPING_FACTOR)));
-                return result.iterator();
-            });
-            input = mapped.reduceByKey((value1, value2) -> {
-                Set<String> finalList = value1.outLinks == null ? value2.outLinks : value1.outLinks;
-                double finalRank = value1.pageRank + value2.pageRank;
-                return new Value(finalList, finalRank);
-            });
-        }
-        return input;
+            } catch (NullPointerException e) {
+//                System.out.println(key._1);
+            }
+            return resultList;
+        });
+
+        result = mapped.reduceByKey((value1, value2) -> value1 + value2);
+        return result;
     }
 
-    void writeToHBase(JavaPairRDD<String, Value> toWrite) {
+    void writeToHBase(JavaPairRDD<String, Integer> toWrite) {
         try {
             Job jobConfig = Job.getInstance(hbaseConf);
             // TODO: 8/11/18 replace test with webpage
             jobConfig.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "test");
             jobConfig.setOutputFormatClass(TableOutputFormat.class);
             JavaPairRDD<ImmutableBytesWritable, Put> hbasePuts = toWrite.mapToPair(pair -> {
-                System.out.println(pair._2.pageRank);
-                Gson gson = new Gson();
                 Put put = new Put(Bytes.toBytes(pair._1));
-                put.addColumn(familyName.getBytes(), outLinksName.getBytes(), gson.toJson(pair._2.outLinks).getBytes());
-                put.addColumn(familyName.getBytes(), pageRankName.getBytes(), Bytes.toBytes(pair._2.pageRank));
+                put.addColumn(familyName.getBytes(), pageRankName.getBytes(), Bytes.toBytes(pair._2));
                 return new Tuple2<>(new ImmutableBytesWritable(), put);
             });
             hbasePuts.saveAsNewAPIHadoopDataset(jobConfig.getConfiguration());
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void close() {
-        if (sparkContext != null)
-            sparkContext.close();
     }
 
 }
--- src/test/java/ir/nimbo/pagerank/RankCalculatorTest.java
+++ src/test/java/ir/nimbo/pagerank/RankCalculatorTest.java
@@ -1,7 +1,16 @@
 package ir.nimbo.pagerank;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.Before;
 import org.junit.Test;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,5 +26,15 @@ public class RankCalculatorTest {
 
     @Test
     public void getResult() {
+//        HashSet<String> outLinkSet = new HashSet<>();
+//        String url ="";
+////        RankCalculator calculator = new RankCalculator("Test", "spark://master-node:9200");
+//        RankCalculator calculator = new RankCalculator();
+//        List <Tuple2<String,HashSet<String>>> list = new ArrayList<>();
+//        SparkConf sc = new SparkConf().setAppName("test").setMaster("spark://master-node:9200");
+//        list.add(new Tuple2<>(url,outLinkSet));
+//        JavaSparkContext sparkContext = new JavaSparkContext(sc);
+//        JavaPairRDD<String, HashSet<String>> input = sparkContext.parallelizePairs(list);
+//        calculator.getResult(input);
     }
 }
